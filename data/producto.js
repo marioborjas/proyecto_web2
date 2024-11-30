55551a8f2b3e2a01a34def7afce3547ce6ce8167
Elmer Hernandez <160553677+MemeH27@users.noreply.github.com>
2024-11-30 10:40:10 -0600

    Agregar los productos

--- data/producto.js
+++ data/producto.js
@@ -1,27 +1,83 @@
 export const productos = [
     {
-      nombre: "Camarón Rojo",
-      descripcion: "Camarones frescos de la más alta calidad.",
-      precio: 19.99,
+      nombre: "Camaron Entero",
+      descripcion: "Camaron que se vende en su forma natural, sin quitarle la cáscara ni la cabeza.",
+      precio: 160,
       imagen: "/imagenes/Camaron-pelado-cocido.png",
       cantidad: 1,
-      detalles: "1 x 250 g"
+      detalles: "1 libra"
     },
     {
-      nombre: "Camarón Blanco",
-      descripcion: "Camarones blancos premium, perfectos para cualquier platillo.",
-      precio: 15.49,
+      nombre: "Camarón Pelado",
+      descripcion: "Es camarón que ha sido despojado de su cáscara, dejando solo la carne.",
+      precio: 220,
       imagen: "/imagenes/Camaron-pelado-cocido.png",
       cantidad: 1,
-      detalles: "1 x 200 g"
+      detalles: "1 libra"
     },
     {
-      nombre: "Camarón en Salsa",
-      descripcion: "Camarones en salsa con un toque especial.",
-      precio: 22.99,
+      nombre: "Camarón con Cola",
+      descripcion: "Camarón que se le ha retirado la cáscara, pero se deja la cola intacta, generalmente para darle un toque estético.",
+      precio: 240,
       imagen: "/imagenes/Camaron-pelado-cocido.png",
       cantidad: 1,
-      detalles: "1 x 300 g"
+      detalles: "1 libra"
+    },
+    {
+      nombre: "Camarón Precocido",
+      descripcion: "Cocinado parcialmente, listo para ser utilizado en recetas o para ser congelado.",
+      precio: 250,
+      imagen: "/imagenes/Camaron-pelado-cocido.png",
+      cantidad: 1,
+      detalles: "1 libra"
+    },
+    {
+      nombre: "Camarón Ahumado o Marinado",
+      descripcion: "Camarón que ha sido sometido a un proceso de ahumado o marinado para darle un sabor distinto.",
+      precio: 300,
+      imagen: "/imagenes/Camaron-pelado-cocido.png",
+      cantidad: 1,
+      detalles: "1 libra"
+    },
+    {
+      nombre: "Cmarón Jumbo",
+      descripcion: "Este es un camarón más grande, ideal para platillos especiales.",
+      precio: 280,
+      imagen: "/imagenes/Camaron-pelado-cocido.png",
+      cantidad: 1,
+      detalles: "1 libra"
+    },
+    {
+      nombre: "Cmarón Extra Grande",
+      descripcion: "26/30 piezas por libra. Más pequeño que el jumbo, pero sifue siendo de buen tamaño.",
+      precio: 240,
+      imagen: "/imagenes/Camaron-pelado-cocido.png",
+      cantidad: 1,
+      detalles: "1 libra"
+    },
+    {
+      nombre: "Cmarón Grande",
+      descripcion: "31/40 piezas por libra. Es el tamaño estándar que se utiliza en muchos platillos.",
+      precio: 220,
+      imagen: "/imagenes/Camaron-pelado-cocido.png",
+      cantidad: 1,
+      detalles: "1 libra"
+    },
+    {
+      nombre: "Cmarón Mediano",
+      descripcion: "41/50 piezas por libra. Este es más pequeño y económico.",
+      precio: 180,
+      imagen: "/imagenes/Camaron-pelado-cocido.png",
+      cantidad: 1,
+      detalles: "1 libra"
+    },
+    {
+      nombre: "Cmarón Pequeño",
+      descripcion: "51/70 piezas por libra. Se utiliza en sopas, caldos o como relleno.",
+      precio: 150,
+      imagen: "/imagenes/Camaron-pelado-cocido.png",
+      cantidad: 1,
+      detalles: "1 libra"
     }
   ];
   
