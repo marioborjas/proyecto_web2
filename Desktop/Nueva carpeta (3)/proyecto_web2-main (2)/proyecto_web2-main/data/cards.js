5e88b297b2af24c96515cb496c5ec856b48e4471
Enoc2077 <Asael5089@gmail.com>
2024-11-30 03:11:33 -0600

    Commit inicial

---
+++ Desktop/Nueva carpeta (3)/proyecto_web2-main (2)/proyecto_web2-main/data/cards.js
@@ -0,0 +1,21 @@
+export const getCardsData = () => [
+    {
+      name: "Camarón entero (sin pelar)",
+      description: "Este es el camarón que se vende en su forma natural, sin quitarle la cáscara ni la cabeza.",
+      price: "Lps.220.00 / Lb",
+      image: "/imagenes/productos/camaron_entero.jpg",
+    },
+    {
+      name: "Camarón pelado (sin cáscara)",
+      description: "Es camarón que ha sido despojado de su cáscara, dejando solo la carne. Perfecto para cocina rapida",
+      price: "Lps.220.00 / Lb",
+      image:  "/imagenes/productos/camaron_pelado.jpg",
+    },
+    {
+      name: "Camarón con cola",
+      description: "Camarón al que se le ha retirado la cáscara, pero se deja la cola intacta, generalmente para darle un toque estético.",
+      price: "Lps.240.00 / Lb",
+      image:  "/imagenes/productos/camaron_con_cola.jpg",
+    },
+  ];
+  
