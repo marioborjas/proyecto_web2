1ea5fdccf4a70080ea8b2eb991d7c03916d99b8b
MemeH27 <yayirobe2305@gmail.com>
2024-12-01 14:31:46 -0600

    Arreglo en productos

--- data/producto.js
+++ data/producto.js
@@ -1,62 +1,62 @@
 export const productos = [
-  {
-      nombre: "Camaron Entero",
-      descripcion: "Camaron que se vende en su forma natural, sin quitarle la cáscara ni la cabeza.",
-      precio: "Lps.160",
-      imagen: "/imagenes/Camaron-pelado-cocido.png"
-  },
-  {
-      nombre: "Camarón Pelado",
-      descripcion: "Es camarón que ha sido despojado de su cáscara, dejando solo la carne.",
-      precio: "Lps.220",
-      imagen: "/imagenes/Camaron-pelado-cocido.png"
-  },
-  {
-      nombre: "Camarón con Cola",
-      descripcion: "Camarón que se le ha retirado la cáscara, pero se deja la cola intacta, generalmente para darle un toque estético.",
-      precio: "Lps.240",
-      imagen: "/imagenes/Camaron-pelado-cocido.png"
-  },
-  {
-      nombre: "Camarón Precocido",
-      descripcion: "Cocinado parcialmente, listo para ser utilizado en recetas o para ser congelado.",
-      precio: "Lps.250",
-      imagen: "/imagenes/Camaron-pelado-cocido.png"
-  },
-  {
-      nombre: "Camarón Ahumado o Marinado",
-      descripcion: "Camarón que ha sido sometido a un proceso de ahumado o marinado para darle un sabor distinto.",
-      precio: "Lps.300",
-      imagen: "/imagenes/Camaron-pelado-cocido.png"
-  },
-  {
-      nombre: "Camarón Jumbo",
-      descripcion: "Este es un camarón más grande, ideal para platillos especiales.",
-      precio: "Lps.280",
-      imagen: "/imagenes/Camaron-pelado-cocido.png"
-  },
-  {
-      nombre: "Camarón Extra Grande",
-      descripcion: "26/30 piezas por libra. Más pequeño que el jumbo, pero sigue siendo de buen tamaño.",
-      precio: "Lps.240",
-      imagen: "/imagenes/Camaron-pelado-cocido.png"
-  },
-  {
-      nombre: "Camarón Grande",
-      descripcion: "31/40 piezas por libra. Es el tamaño estándar que se utiliza en muchos platillos.",
-      precio: "Lps.220",
-      imagen: "/imagenes/Camaron-pelado-cocido.png"
-  },
-  {
-      nombre: "Camarón Mediano",
-      descripcion: "41/50 piezas por libra. Este es más pequeño y económico.",
-      precio: "Lps.180",
-      imagen: "/imagenes/Camaron-pelado-cocido.png"
-  },
-  {
-      nombre: "Camarón Pequeño",
-      descripcion: "51/70 piezas por libra. Se utiliza en sopas, caldos o como relleno.",
-      precio: "Lps.150",
-      imagen: "/imagenes/Camaron-pelado-cocido.png"
-  }
-];
+    {
+        nombre: "Camaron Entero",
+        descripcion: "Camaron que se vende en su forma natural, sin quitarle la cáscara ni la cabeza.",
+        precio: "Lps.160",
+        imagen: "/imagenes/productos/camaron_entero.jpg"
+    },
+    {
+        nombre: "Camarón Pelado",
+        descripcion: "Es camarón que ha sido despojado de su cáscara, dejando solo la carne.",
+        precio: "Lps.220",
+        imagen: "/imagenes/productos/camaron_pelado.jpg"
+    },
+    {
+        nombre: "Camarón con Cola",
+        descripcion: "Camarón que se le ha retirado la cáscara, pero se deja la cola intacta, generalmente para darle un toque estético.",
+        precio: "Lps.240",
+        imagen: "/imagenes/productos/camaron_con_cola.jpg"
+    },
+    {
+        nombre: "Camarón Precocido",
+        descripcion: "Cocinado parcialmente, listo para ser utilizado en recetas o para ser congelado.",
+        precio: "Lps.250",
+        imagen: "/imagenes/productos/camaron_precocido.jpg"
+    },
+    {
+        nombre: "Camarón Ahumado o Marinado",
+        descripcion: "Camarón que ha sido sometido a un proceso de ahumado o marinado para darle un sabor distinto.",
+        precio: "Lps.300",
+        imagen: "/imagenes/productos/camaron_ahumado.jpg"
+    },
+    {
+        nombre: "Camarón Jumbo",
+        descripcion: "Este es un camarón más grande, ideal para platillos especiales.",
+        precio: "Lps.280",
+        imagen: "/imagenes/productos/camaron_jumbo.jpg"
+    },
+    {
+        nombre: "Camarón Extra Grande",
+        descripcion: "26/30 piezas por libra. Más pequeño que el jumbo, pero sigue siendo de buen tamaño.",
+        precio: "Lps.240",
+        imagen: "/imagenes/productos/camaron_extra_grande.jpg"
+    },
+    {
+        nombre: "Camarón Grande",
+        descripcion: "31/40 piezas por libra. Es el tamaño estándar que se utiliza en muchos platillos.",
+        precio: "Lps.220",
+        imagen: "/imagenes/productos/camaron_grande.jpg"
+    },
+    {
+        nombre: "Camarón Mediano",
+        descripcion: "41/50 piezas por libra. Este es más pequeño y económico.",
+        precio: "Lps.180",
+        imagen: "/imagenes/productos/camaron_mediano.jpg"
+    },
+    {
+        nombre: "Camarón Pequeño",
+        descripcion: "51/70 piezas por libra. Se utiliza en sopas, caldos o como relleno.",
+        precio: "Lps.150",
+        imagen: "/imagenes/productos/camaron_pequeno.png"
+    }
+  ];
